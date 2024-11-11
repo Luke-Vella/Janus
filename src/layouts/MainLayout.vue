5aaffcfb33b1b0d518bef7fb1905e12425efbb45
Luke Vella <luke.vella.0697@gmail.com>
2024-11-11 20:06:05 +0100

    Stared Profile

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -50,41 +50,46 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+  </q-layout>
 
-    <q-dialog v-model="dialog" position="bottom">
+<q-dialog v-model="dialog" position="bottom">
 
-      <q-card style="width: 450px">
+    <q-card style="width: 450px">
+
         <q-linear-progress :value="1" color="white" />
 
         <q-card-section class="row items-center no-wrap">
 
         <q-icon class="q-mr-md" size="lg" name="badge" />
 
-          <div>
-            <div class="text-weight-bold">bidwi_widbi</div>
-            <div class="text-grey">Research Intern</div>
-            <q-linear-progress size="5px" :value=".1" color="purple">
-            </q-linear-progress>
+            <div>
 
-            <div class="text-grey-7 text-caption q-mt-sm">
+                <div class="text-weight-bold">bidwi_widbi</div>
 
-              <div>Current Status: <span class="text-orange">Idle</span></div>
+                <div class="text-grey">Research Intern</div>
 
-              <div>Clearance Level: <span class="text-white">WHITE</span></div>
+                <q-linear-progress size="5px" :value=".1" color="purple">
+                </q-linear-progress>
 
-              <div>
-                Authorisation Token: <span class="text-grey-1">227FF-1127S-SDG12-VCVJS</span>
-                <q-btn class="q-mx-sm q-px-sm" size="xs" flat icon="content_copy" />
-              </div>
+                <div class="text-grey-7 text-caption q-mt-sm">
+
+                    <div>Current Status: <span class="text-orange">Idle</span></div>
+
+                    <div>Clearance Level: <span class="text-white">WHITE</span></div>
+
+                    <div>
+                    Authorisation Token: <span class="text-grey-1">227FF-1127S-SDG12-VCVJS</span>
+                    <q-btn class="q-mx-sm q-px-sm" size="xs" flat icon="content_copy" />
+                    </div>
+
+                </div>
 
             </div>
-          </div>
 
         </q-card-section>
-      </q-card>
-    </q-dialog>
 
-  </q-layout>
+    </q-card>
+</q-dialog>
 </template>
 
 <script setup lang="ts">
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,7 +1,129 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    Profile
-  </q-page>
+    <q-page>
+        <div class="q-pa-md">
+            <div class="row items-start q-col-gutter-md ">
+
+                <!-- PROFILE SECTION -->
+                <div class="col-12 col-md-4 full-height">
+                    <q-card flat bordered class="my-card">
+                        <q-item>
+                            <q-item-section avatar>
+                                <q-avatar size="80px">
+                                    <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                                </q-avatar>
+                            </q-item-section>
+
+                            <q-item-section>
+                                <q-item-label>bidwi_widbi</q-item-label>
+
+                                <q-linear-progress :value=".1" color="purple" />
+
+                                <q-item-label caption>
+                                    Research - Research Intern
+                                </q-item-label>
+                            </q-item-section>
+                        </q-item>
+
+                        <q-separator class="q-my-md" />
+
+                        <q-item>
+                            <q-item-section>
+                                <q-item-label  overline>Employee Overview</q-item-label>
+                            </q-item-section>
+                        </q-item>
+
+                        <q-item>
+                            <q-item-section>
+
+                                <q-item-label>Hunger Level</q-item-label>
+
+                                <q-linear-progress :value=".1" color="blue" />
+
+                                <q-item-label caption>
+                                </q-item-label>
+                            </q-item-section>
+                        </q-item>
+
+                        <q-item>
+                            <q-item-section>
+                                <q-item-label>Sanity</q-item-label>
+
+                                <q-linear-progress :value="1" color="blue" />
+
+                                <q-item-label caption>
+                                </q-item-label>
+                            </q-item-section>
+                        </q-item>
+
+                        <q-item>
+                            <q-item-section>
+                                <q-item-label>Sleep Deprivation</q-item-label>
+
+                                <q-linear-progress :value=".2" color="blue" />
+
+                                <q-item-label caption>
+                                </q-item-label>
+                            </q-item-section>
+                        </q-item>
+
+                        <q-separator class="q-my-md" />
+
+                        <q-item>
+                            <q-item-section>
+                                <q-item-label  overline>Employee Status</q-item-label>
+                            </q-item-section>
+                        </q-item>
+
+                        <q-item>
+                            <q-item-section>
+                                <q-item-label>Status Effects</q-item-label>
+
+                                <q-item-label caption>
+                                    None
+                                </q-item-label>
+                            </q-item-section>
+                        </q-item>
+
+                        <q-item>
+                            <q-item-section>
+                                <q-item-label>Latest Psychiatric Evaluation ( 05/08/2024 ) </q-item-label>
+
+                                <q-item-label caption>
+                                    All Clear
+                                </q-item-label>
+
+                            </q-item-section>
+                        </q-item>
+
+                        <q-item>
+                            <q-item-section>
+                                <q-item-label>Latest Physical ( 11/10/2024 )</q-item-label>
+
+                                <q-item-label caption>
+                                    All Clear
+                                </q-item-label>
+
+                            </q-item-section>
+                        </q-item>
+
+                    </q-card>
+                </div>
+
+                <div class="col-12 col-md-8">
+                    <q-card flat bordered class="col-8 my-card">
+                        <q-item>
+                            <q-item-section>
+                                <q-item-label>Career Statistics</q-item-label>
+                                <q-item-label caption>
+                                Subhead
+                                </q-item-label>
+                            </q-item-section>
+                        </q-item>
+                    </q-card>
+                </div>
+            </div>
+        </div>
+    </q-page>
 </template>
 
 <script setup lang="ts">
@@ -9,3 +131,11 @@ defineOptions({
   name: 'My-Profile'
 })
 </script>
+
+<style lang="scss">
+
+.my-card {
+    padding: 15px;
+}
+
+</style>
